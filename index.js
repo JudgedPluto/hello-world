503b0790019259c7e6b8b9f7918ace39d6d020ac
pawanvimukthi <judgedpluto3156@gmail.com>
2022-11-18 14:35:20 +0530

    Custom env variables

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require("http");
 
 const requestListener = function (req, res) {
   if (req.method !== "GET") return res.writeHead(405).end("Method Not Allowed");
-  res.writeHead(200).end("Hello, World!");
+  res.writeHead(200).end(process.env.MESSAGE);
 };
 
 const server = http.createServer(requestListener);
